2e07abbabf893e3004f8a2dd2de5bca30044a304
Ethan Louw <160718170+eclouw@users.noreply.github.com>
2024-11-27 21:34:32 +0200

    changes to the data is updated locally

--- src/Pages/EmployeesTableView.js
+++ src/Pages/EmployeesTableView.js
@@ -2,18 +2,27 @@ import {Table, Header, HeaderRow, Body, Row, HeaderCell, Cell} from '@table-libr
 import {HeaderCellSelect, CellSelect, SelectClickTypes, SelectTypes, useRowSelect} from "@table-library/react-table-library/select";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Spinner from 'react-bootstrap/Spinner';
+import Container from 'react-bootstrap/Container';
+import {default as Brow} from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { useTheme } from '@table-library/react-table-library/theme';
 import { getTheme } from '@table-library/react-table-library/baseline';
 import getData from '../Components/getData';
 import axios from "react-axios";
 import { useState } from 'react';
 import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function EmployeesTableView(){
     const [employeeData, setEmployeeData] = useState([]);
     const [loadingEmployeeData, setLoadingEmployeeData] = useState(true);
     const [tableData, setTableData] = useState({nodes : []})
     const [selectedEmployee, setSelectedEmployee] = useState([]);
+
+    //States for the currently selected employee details
+    const [inputEmployeeFirstName, setInputEmployeeFirstName] = useState('');
     
       
       //Get Employee Data
@@ -64,16 +73,31 @@ function EmployeesTableView(){
       console.log(tableData)
       findSelectedEmployee(state.id)
       
+      
     }
 
     function findSelectedEmployee(id){
       setSelectedEmployee(
         tableData.nodes.find((item)=>item.emp_number === id)
       )
+      
     }
 
-
-    
+    function updateEmployeeDetails(){
+      const index = tableData.nodes.findIndex((item)=> item.id === selectedEmployee.id);
+      
+      let employees = [...tableData.nodes];
+      
+      let employee = {...employees[index]}
+      
+      //update the details in the data to what the user has inputed
+      employee.first_name = document.getElementById('input_first_name').value;
+      employee.last_name = document.getElementById('input_last_name').value;
+      employee.email = document.getElementById('input_email').value;
+      employees[index] = employee;
+      
+      setTableData({nodes: employees});
+    }
 
     const data = tableData;
 
@@ -87,7 +111,7 @@ function EmployeesTableView(){
     return (
     <div className='EmployeesTable'>
         {loadingEmployeeData || tableData.nodes.length===0 ?(
-          <p>loading</p>
+          <Spinner animation='border'/>
         ):(
           <>
           <Table data={data} theme={theme} select={sel}>
@@ -143,12 +167,62 @@ function EmployeesTableView(){
               )
             }
           </Table>
-          <p>{selectedEmployee.first_name}</p>
+          <p>Currently Selected Employee with ID:{selectedEmployee.emp_number}</p>
+          <div>
+          <Container>
+            <Brow>
+              <Col>
+                <p>First Name 
+                <input type="text" defaultValue={selectedEmployee.first_name} id="input_first_name"/>
+                </p>
+              </Col>
+              <Col>
+                <p>Last Name 
+                <input type="text" defaultValue={selectedEmployee.last_name} id="input_last_name"/>
+                </p>
+              </Col>
+              <Col>
+                <p>Email 
+                <input type="text" defaultValue={selectedEmployee.email} id="input_email"/>
+                </p>
+              </Col>
+            </Brow>
+            <Brow>
+              <Col>
+                <p>Role
+                <input type="text" defaultValue={selectedEmployee.role_name}/>
+                </p>
+              </Col>
+              <Col>
+                <p>Salary
+                <input type="text" defaultValue={selectedEmployee.salary}/>
+                </p>
+              </Col>
+              <Col>
+                <p>Birth Date
+                <input type="text" defaultValue={selectedEmployee.birthdate}/>
+                </p>
+              </Col>
+            </Brow>
+            <Brow>
+            <Col>
+            <p>
+              Line Manager
+              <input type="text" defaultValue={selectedEmployee.manager_name}/>
+            </p>
+            </Col>
+            <Col>
+            Select Line Manager
+            </Col>
+            </Brow>
+            <Brow>
+            <button onClick={updateEmployeeDetails}>Submit Changes</button>
+            </Brow>
+          </Container>
+        </div>
           </>
         )}
-        <div>
         
-        </div>
     </div>
     );
 }
