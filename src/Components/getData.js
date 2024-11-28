c5a904d214f8a1043a5fd0d699b60636602d5c01
Ethan Louw <160718170+eclouw@users.noreply.github.com>
2024-11-28 18:31:27 +0200

    updated data fetching component to also fetch roles from the database

--- src/Components/getData.js
+++ src/Components/getData.js
@@ -10,13 +10,14 @@ const getData = async(table)=>{
         if (table=="employees"){
             const response = await fetchData(url+':'+port+"/get/employees");
             return response;
+        }else if (table=="roles"){
+            const response = await fetchData(url+':'+port+"/get/roles")
+            return response;
         }
     }
 
     const fetchData=async(command)=>{
         const dataResponse = await axios.get(command);
-
-        
         return dataResponse.data;
     }
     
