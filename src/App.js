98053c542a1248d3635e581b0f10effec8af6cd2
Ethan Louw <160718170+eclouw@users.noreply.github.com>
2024-11-30 14:57:08 +0200

    Created and added a navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import './App.css';
 import EmployeesTableView from './Pages/EmployeesTableView';
 import EmployeeCreator from './Pages/EmployeeCreator';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import NavBar from './Components/UI/NavBar';
 
 function App() {
   return (
+    <>
     <BrowserRouter>
+    <NavBar/>
+    <div class="container">
       <Routes>
-        <Route path="/edit/Employee" Component={EmployeesTableView}/>
-        <Route path="/create/Employee" Component={EmployeeCreator}/>
+      <Route path="/" element={<HomePage/>}/>
+        <Route path="/edit/Employee" element={<EmployeesTableView/>}/>
+        <Route path="/create/Employee" element={<EmployeeCreator/>}/>
       </Routes>
+      </div>
     </BrowserRouter>
+    </>
   );
 }
 
